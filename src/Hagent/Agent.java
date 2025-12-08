7014115fd5b86948c16ebc7e11c130ef377820b7
Louis Chouane <chouane.louis@gmail.com>
2025-12-08 17:08:36 +0100

    creation interface agent

--- src/Hagent/Agent.java
+++ src/Hagent/Agent.java
@@ -1,17 +1,10 @@
 package src.Hagent;
 
-import java.util.Hashtable;
 
 public interface Agent extends java.io.Serializable {
     public void init(String name, Node origin);
 
-    public void setNameServer(Hashtable<String, Object> ns);
+    public void setup(Server localServer);
 
-    public Hashtable<String, Object> getNameServer();
-
-    public void move(Node target) throws MoveException;
-
-    public void back() throws MoveException;
-
-    public void main() throws MoveException;
+    public void run();
 }
--- src/Hagent/AgentAbs.java
+++ src/Hagent/AgentAbs.java
@@ -1,5 +1,20 @@
 package src.Hagent;
 
-public abstract class AgentAbs implements Agent {
+public abstract class AgentAbs implements Agent, Serializible {
+
+    private int state = 0;
+
+    public void init(String name, Node origin) {}
+
+    public void setup(Hashtable<String, Object> ns) {}
+
+    public void run() {}
+
+    private abstract void runState(int state);
+
+    private void move(Node target, int newState) {}
+
+    private void finish() {}
+
 
 }
