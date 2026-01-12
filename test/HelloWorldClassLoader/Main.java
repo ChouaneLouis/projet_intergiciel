3879522981d1beb6445285505af8b7ff1a54a771
chloeboireaudevier <cbd65@hotmail.fr>
2026-01-12 15:50:10 +0100

    fix example

--- test/HelloWorldClassLoader/Main.java
+++ test/HelloWorldClassLoader/Main.java
@@ -1,9 +1,11 @@
 package HelloWorldClassLoader;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
@@ -29,15 +31,39 @@ public class Main {
             agent.addKnownNode(servNode);
             agent.run();
 
+            System.out.println("Waiting....");
             Socket s = ss.accept();
+            System.out.println("Recieved agent");
+
             InputStream inputIS = s.getInputStream();
+
+            // deserialize
             ObjectInputStream ois = new ObjectInputStream(inputIS);
+
             Map<String, byte[]> dataMap = (Map<String, byte[]>) ois.readObject();
+            Loader loader = new Loader();
+            for (String className : dataMap.keySet()) {
+
+                loader.addClass(className, dataMap.get(className));
+            }
+
             InputStream is = s.getInputStream();
             DataInputStream in = new DataInputStream(is);
             int objLen = in.readInt();
             byte[] objBytes = in.readNBytes(objLen);
+
+            ois = new ObjectInputStream(
+                    new ByteArrayInputStream(objBytes)) {
+
+                @Override
+                protected Class<?> resolveClass(ObjectStreamClass desc)
+                        throws IOException, ClassNotFoundException {
+                    return loader.loadClass(desc.getName());
+                }
+            };
+
             Agent ag = (Agent) ois.readObject();
+
             System.out.println("Agent " + ag.getName() + " received at destination:");
             if (ag.getExceptionRecord() != null) {
                 System.out.println("Agent Exception Record:");
